4693e8b3f5c0748ad0df80ef27e7dcc937e16a91
tdjey33 <shfartem@mail.ru>
2025-01-12 00:22:47 +0300

    string review

--- string.h
+++ string.h
@@ -5,8 +5,8 @@
 class CowString {
     size_t capacity_ = 1;
     size_t size_ = 0;
-    char* s_ = nullptr;
-    int* instance_counter_ = nullptr;
+    char* str_ = nullptr;
+    size_t* instance_counter_ = nullptr;
 
   private:
     void self_copy(size_t);
@@ -24,13 +24,8 @@ class CowString {
     CowString(size_t, char);
     CowString();
     CowString(const CowString&);
-    CowString& operator=(CowString);
+    CowString& operator=(const CowString&);
     ~CowString();
-    friend bool operator==(const CowString&, const CowString&);
-    friend bool operator<(const CowString&, const CowString&);
-    friend bool operator>(const CowString&, const CowString&);
-    friend bool operator<=(const CowString&, const CowString&);
-    friend bool operator>=(const CowString&, const CowString&);
     char& operator[](int idx);
     const char& operator[](int idx) const;
     size_t length() const;
@@ -49,27 +44,25 @@ class CowString {
     CowString operator+(const char*) const;
     size_t find(const CowString&) const;
     size_t rfind(const CowString&) const;
-    CowString substr(int, unsigned long) const;
+    CowString substr(size_t, size_t) const;
     bool empty() const;
     void clear();
     void shrink_to_fit();
     char* data();
     const char* data() const;
-    friend std::istream& operator>>(std::istream&, CowString&);
-    friend std::ostream& operator<<(std::ostream&, const CowString&);
+};
 
-    friend CowString operator+(char c, const CowString& str) {
-        CowString result{1 + str.size_, '0'};
-        result[0] = c;
-        memcpy(&result.s_[1], str.s_, str.size_);
-        return result;
-    };
+CowString operator+(char c, const CowString& str) {
+    CowString result{1 + str.size(), '0'};
+    result[0] = c;
+    memcpy(result.data() + 1, str.data(), str.size());
+    return result;
+};
 
-    friend CowString operator+(const char* other_s, const CowString& str) {
-        CowString result(other_s);
-        result += str;
-        return result;
-    };
+CowString operator+(const char* other_s, const CowString& str) {
+    CowString result(other_s);
+    result += str;
+    return result;
 };
 
 std::istream& operator>>(std::istream& input, CowString& inp_str) {
@@ -88,22 +81,22 @@ std::istream& operator>>(std::istream& input, CowString& inp_str) {
 }
 
 std::ostream& operator<<(std::ostream& output, const CowString& out_str) {
-    output.write(out_str.s_, static_cast<long>(out_str.size_));
+    output.write(out_str.data(), static_cast<long>(out_str.size()));
     return output;
 }
 
 void CowString::self_copy(size_t new_capacity) {
     --(*instance_counter_);
-    char* data_pointer = new char[sizeof(int) + new_capacity + 1];
+    char* data_pointer = new char[sizeof(size_t) + new_capacity + 1];
     char* new_s = reinterpret_cast<char*>(data_pointer);
-    new_s += sizeof(int);
-    memcpy(new_s, s_, size_);
-    s_ = new_s;
-    s_[size_] = 0;
+    new_s += sizeof(size_t);
+    memcpy(new_s, str_, size_);
+    str_ = new_s;
+    str_[size_] = 0;
     if (*instance_counter_ == 0) {
         delete[] reinterpret_cast<char*>(instance_counter_);
     }
-    instance_counter_ = reinterpret_cast<int*>(data_pointer);
+    instance_counter_ = reinterpret_cast<size_t*>(data_pointer);
     *instance_counter_ = 1;
     capacity_ = new_capacity;
 }
@@ -112,48 +105,45 @@ void CowString::swap(CowString& other) {
     using std::swap;
     swap(size_, other.size_);
     swap(capacity_, other.capacity_);
-    swap(s_, other.s_);
+    swap(str_, other.str_);
     swap(instance_counter_, other.instance_counter_);
 }
 
 CowString::CowString(size_t capacity, size_t size = 0)
     : capacity_(capacity), size_(size) {
-    char* data_pointer = new char[sizeof(int) + capacity_ + 1];
-    instance_counter_ = reinterpret_cast<int*>(data_pointer);
+    char* data_pointer = new char[sizeof(size_t) + capacity_ + 1];
+    instance_counter_ = reinterpret_cast<size_t*>(data_pointer);
     *instance_counter_ = 1;
-    s_ = reinterpret_cast<char*>(data_pointer);
-    s_ += sizeof(int);
-    s_[size_] = 0;
+    str_ = reinterpret_cast<char*>(data_pointer);
+    str_ += sizeof(size_t);
+    str_[size_] = 0;
 }
 
 CowString::CowString(const char* other_s) : CowString(strlen(other_s) + 1) {
-    int symbols_cnt = 0;
-    while (other_s[symbols_cnt] != '\0') {
-        symbols_cnt++;
-    }
-    capacity_ = symbols_cnt;
-    size_ = symbols_cnt;
-    memcpy(this->s_, other_s, size_ + 1);
+    --capacity_;
+    size_ = capacity_;
+    memcpy(this->str_, other_s, size_ + 1);
 }
 
 CowString::CowString(size_t n, char c) : CowString(n, n) {
-    std::fill(s_, s_ + n, c);
-    s_[size_] = 0;
+    std::fill(str_, str_ + n, c);
+    str_[size_] = 0;
 }
 
 CowString::CowString() : CowString(1) {}
 
 CowString::CowString(const CowString& other)
-    : capacity_(other.capacity_),
-      size_(other.size_),
-      instance_counter_(other.instance_counter_) {
+    : capacity_(other.capacity_)
+    , size_(other.size_)
+    , str_(other.str_)
+    , instance_counter_(other.instance_counter_) {
     ++(*instance_counter_);
-    s_ = other.s_;
 }
 
-CowString& CowString::operator=(CowString other) {
+CowString& CowString::operator=(const CowString& other) {
     if (this != &other) {
-        this->swap(other);
+        CowString copied(other);
+        this->swap(copied);
     }
     return *this;
 }
@@ -166,40 +156,40 @@ CowString::~CowString() {
     }
 }
 
-bool operator==(const CowString& a, const CowString& b) {
-    if (a.size_ != b.size_) return false;
-    if (a.s_ == b.s_) return true;
-    return memcmp(a.s_, b.s_, a.size_) == 0;
+bool operator==(const CowString& lhs, const CowString& rhs) {
+    return (lhs.data() == rhs.data()) ||
+           ((lhs.size() == rhs.size()) &&
+            memcmp(lhs.data(), rhs.data(), lhs.size()) == 0);
 }
 
-bool operator<(const CowString& a, const CowString& b) {
-    if (a.s_ == b.s_) return false;
-    size_t border = std::min(a.size_, b.size_);
-    int val = memcmp(a.s_, b.s_, border);
-    return val < 0 || (val == 0 && a.size_ < b.size_);
+bool operator<(const CowString& lhs, const CowString& rhs) {
+    size_t border = std::min(lhs.size(), rhs.size());
+    int val = memcmp(lhs.data(), rhs.data(), border);
+    return (lhs.data() != rhs.data()) &&
+           (val < 0 || (val == 0 && lhs.size() < rhs.size()));
 }
 
-bool operator>(const CowString& a, const CowString& b) {
-    return b < a;
+bool operator>(const CowString& lhs, const CowString& rhs) {
+    return rhs < lhs;
 }
 
-bool operator<=(const CowString& a, const CowString& b) {
-    return !(a > b);
+bool operator<=(const CowString& lhs, const CowString& rhs) {
+    return !(lhs > rhs);
 }
 
-bool operator>=(const CowString& a, const CowString& b) {
-    return !(a < b);
+bool operator>=(const CowString& lhs, const CowString& rhs) {
+    return !(lhs < rhs);
 }
 
 char& CowString::operator[](int idx) {
     if (*instance_counter_ != 1) {
         self_copy(capacity_);
     }
-    return s_[idx];
+    return str_[idx];
 }
 
 const char& CowString::operator[](int idx) const {
-    return s_[idx];
+    return str_[idx];
 }
 
 size_t CowString::length() const {
@@ -228,31 +218,31 @@ void CowString::push_back(char c) {
         self_copy(new_capacity);
     }
 
-    s_[size_++] = c;
-    s_[size_] = '\0';
+    str_[size_++] = c;
+    str_[size_] = '\0';
 }
 
 void CowString::pop_back() {
     if (*instance_counter_ != 1) self_copy();
-    s_[--size_] = '\0';
+    str_[--size_] = '\0';
 }
 
 const char& CowString::front() const {
-    return s_[0];
+    return str_[0];
 }
 
 char& CowString::front() {
     if (*instance_counter_ != 1) self_copy();
-    return s_[0];
+    return str_[0];
 }
 
 const char& CowString::back() const {
-    return s_[size_ - 1];
+    return str_[size_ - 1];
 }
 
 char& CowString::back() {
     if (*instance_counter_ != 1) self_copy();
-    return s_[size_ - 1];
+    return str_[size_ - 1];
 }
 
 CowString& CowString::operator+=(char c) {
@@ -264,7 +254,7 @@ CowString& CowString::operator+=(const CowString& other) {
     bool need_realloc = false;
     size_t new_capacity = capacity_;
     if (size_ + other.size_ > capacity_) {
-        new_capacity = size_ + other.size_;
+        new_capacity = (size_ + other.size_) * 2;
         need_realloc = true;
     } else if (*instance_counter_ != 1) {
         need_realloc = true;
@@ -273,16 +263,15 @@ CowString& CowString::operator+=(const CowString& other) {
     if (need_realloc) {
         self_copy(new_capacity);
     }
-    memcpy(s_ + size_, other.s_, other.size_);
+    memcpy(str_ + size_, other.str_, other.size_);
     size_ += other.size_;
-    s_[size_] = '\0';
+    str_[size_] = '\0';
     return *this;
 }
 
 CowString CowString::operator+(const CowString& other) const {
-    CowString result{size_ + other.size_, '0'};
-    memcpy(result.s_, s_, size_);
-    memcpy(result.s_ + size_, other.s_, other.size_);
+    CowString result(*this);
+    result += other;
     return result;
 }
 
@@ -303,7 +292,7 @@ size_t CowString::find(const CowString& other) const {
         return size_;
     }
     for (size_t i = 0; i <= size_ - other.size_; ++i) {
-        if (memcmp(s_ + i, other.s_, other.size_) == 0) {
+        if (memcmp(str_ + i, other.str_, other.size_) == 0) {
             return i;
         }
     }
@@ -316,23 +305,22 @@ size_t CowString::rfind(const CowString& other) const {
     }
 
     for (int i = static_cast<int>(size_ - other.size_); i >= 0; --i) {
-        if (memcmp(s_ + i, other.s_, other.size_) == 0) {
+        if (memcmp(str_ + i, other.str_, other.size_) == 0) {
             return i;
         }
     }
     return size_;
 }
 
-CowString CowString::substr(int start, unsigned long count) const {
-    if (count == 0 || (size_t)start >= size_) {
+CowString CowString::substr(size_t start, size_t count) const {
+    if (count == 0 || start >= size_) {
         return {};
     }
-    if (count > size_ - (size_t)start) {
+    if (count > size_ - start) {
         count = size_ - start;
     }
-
-    CowString result{(size_t)count, '0'};
-    memcpy(result.s_, s_ + start, count);
+    CowString result{count, '0'};
+    memcpy(result.str_, str_ + start, count);
     return result;
 }
 
@@ -343,7 +331,7 @@ bool CowString::empty() const {
 void CowString::clear() {
     if (*instance_counter_ != 1) self_copy();
     size_ = 0;
-    s_[size_] = '\0';
+    str_[size_] = '\0';
 }
 
 void CowString::shrink_to_fit() {
@@ -357,14 +345,14 @@ void CowString::shrink_to_fit() {
     }
 
     if (need_realloc) self_copy(new_capacity);
-    s_[size_] = '\0';
+    str_[size_] = '\0';
 }
 
 char* CowString::data() {
     if (*instance_counter_ != 1) self_copy();
-    return s_;
+    return str_;
 }
 
 const char* CowString::data() const {
-    return s_;
+    return str_;
 }
